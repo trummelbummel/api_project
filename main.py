82239956116c977da3c43c8d428c49e09809373a
Theresa Fruhwuerth (TV-D-11) <Theresa.Fruhwuerth@bmw.de>
2023-05-05 14:20:59 +0200

    add code for the project

--- main.py
+++ main.py
@@ -1,1 +1,93 @@
 # Put the code for your API here.
+import pickle
+from typing import Any
+
+import pandas as pd
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
+
+from .ml.data import process_data
+
+app = FastAPI()
+
+MODEL = pickle.load(open('./model/random_forest.pickle', 'rb'))
+ENCODER = pickle.load(open('./model/encoder.pickle', 'rb'))
+LB = pickle.load(open('./model/labelbinarizer.pickle', 'rb'))
+CAT_FEATURES = [
+    "workclass",
+    "education",
+    "marital_status",
+    "occupation",
+    "relationship",
+    "race",
+    "sex",
+    "native_country",
+]
+
+class ResponseItem(BaseModel):
+    status_code: int
+    predictions: list[str, None] = []
+
+
+class CensusData(BaseModel):
+    age: int
+    workclass: str
+    fnlgt: int
+    education: str
+    education_num: int = Field(alias=' education-num')
+    marital_status: str = Field(alias=' marital-status')
+    occupation: str
+    relationship: str
+    race: str
+    sex: str
+    capital_gain: int = Field(alias=' capital-gain')
+    capital_loss: int = Field(alias=' capital-loss')
+    hours_per_week: int = Field(alias=' hours-per-week')
+    native_country: str = Field(alias=' native-country')
+
+    class Config:
+        schema_extra = {
+            "example": {'age': 39,
+                        'workclass': 'State-gov',
+                        'fnlgt': 77516,
+                        'education':
+                            'Bachelors',
+                        'education_num': 13,
+                        'marital-status':
+                            'Never-married',
+                        'occupation':
+                            'Adm-clerical',
+                        'relationship':
+                            'Not-in-family',
+                        'race': 'White',
+                        'sex': 'Male',
+                        'capital-gain': 2174,
+                        'capital-loss': 0,
+                        'hours-per-week': 40,
+                        'native-country': 'United-States',
+                        }
+
+        }
+
+
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the API for census data based salary prediction."}
+
+
+@app.post("/predict", response_model=ResponseItem)
+async def predict(input: CensusData) -> Any:
+    try:
+        x = pd.DataFrame([dict(input)])
+        x_preprocessed, y, _, _ = process_data(x,
+                                               categorical_features=CAT_FEATURES,
+                                               label=None,
+                                               training=False,
+                                               encoder=ENCODER,
+                                               lb=LB
+                                               )
+        pred = MODEL.predict(x_preprocessed)
+        predictions = LB.inverse_transform(pred)
+        return ResponseItem(status_code=200, predictions=predictions.tolist())
+    except BaseException:
+        return ResponseItem(status_code=503, predictions=[None])
--- train_model.py
+++ train_model.py
@@ -1,28 +1,56 @@
 # Script to train machine learning model.
-
+from copy import deepcopy
+import logging
+import pandas as pd
 from sklearn.model_selection import train_test_split
+
+from ml.data import clean_data, process_data
+from ml.model import compute_sliced_metrics, store_model, train_model
 
 # Add the necessary imports for the starter code.
 
 # Add code to load in the data.
+data = pd.read_csv('./data/census.csv')
 
-# Optional enhancement, use K-fold cross validation instead of a train-test split.
+data = clean_data(data)
+# Optional enhancement, use K-fold cross validation instead of a
+# train-test split.
 train, test = train_test_split(data, test_size=0.20)
 
 cat_features = [
     "workclass",
     "education",
-    "marital-status",
+    "marital_status",
     "occupation",
     "relationship",
     "race",
     "sex",
-    "native-country",
+    "native_country",
 ]
+
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
+    deepcopy(train),
+    categorical_features=cat_features,
+    label="salary",
+    training=True
 )
 
 # Proces the test data with the process_data function.
-
+X_test, y_test, _, _ = process_data(
+    deepcopy(test),
+    categorical_features=cat_features,
+    label="salary",
+    training=False,
+    encoder=encoder,
+    lb=lb
+)
 # Train and save a model.
+
+model = train_model(X_train, y_train)
+X_test = pd.DataFrame(X_test)
+X_test['race'] = test['race']
+compute_sliced_metrics(X_test, y_test, model, category='race')
+
+store_model(encoder, './model/encoder.pickle')
+store_model(lb, './model/labelbinarizer.pickle')
+store_model(model, './model/random_forest.pickle')
