06804b7c9a57e1ed31d7f35a82d54ddd1a80b0d5
Theresa Fruhwuerth (TV-D-11) <Theresa.Fruhwuerth@bmw.de>
2023-05-07 13:38:13 +0200

    add fixes and add live requests.py file
    add liverequest file

--- live_request.py
+++ live_request.py
@@ -1,14 +1,13 @@
 import requests
 
 inputs = {'age': 48, 'workclass': 'Private', 'fnlgt': 45612,
-               'education': 'HS-grad',
-               ' education-num': 9, ' marital-status': 'Never-married',
-               'occupation': 'Adm-clerical',
-               'relationship': 'Unmarried', 'race': 'Black',
-               'sex': 'Female', ' capital-gain': 0, ' capital-loss': 0,
-               ' hours-per-week': 37, ' native-country': 'United-States'}
-
+          'education': 'HS-grad',
+          ' education-num': 9, ' marital-status': 'Never-married',
+          'occupation': 'Adm-clerical',
+          'relationship': 'Unmarried', 'race': 'Black',
+          'sex': 'Female', ' capital-gain': 0, ' capital-loss': 0,
+          ' hours-per-week': 37, ' native-country': 'United-States'}
 
 url = 'https://udactiy-api.onrender.com/'
-x = requests.post(url, json=inputs, proxies=proxies, verify=True)
+x = requests.post(url, json=inputs, verify=True)
 print(x)
